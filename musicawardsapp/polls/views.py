e469d4bb7a6890a92c29b0332fd44802f6306ff0
Martin Mendez <martinpercu@gmail.com>
2022-07-26 23:18:23 +0200

    forms first part OK

--- musicawardsapp/polls/views.py
+++ musicawardsapp/polls/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 
-from .models import Question
+from .models import Question, Choice
 
 # Create your views here.
 def index(request):
@@ -25,7 +26,18 @@ def result(request, question_id):
 
 
 def vote(request, question_id):
-    return HttpResponse(f'You are voting for question N° {question_id}')
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, "polls/detail.html", {
+            "question": question,
+            "error_message": "You don't choose an answer. Please do it"            
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse("polls:result", args=(question.id,)))
 
 
 
