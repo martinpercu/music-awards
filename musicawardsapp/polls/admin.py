87b86a4f7bb26aeaa5baee6933f8080f1866ba94
Martin Mendez <martinpercu@gmail.com>
2022-07-30 17:48:40 +0200

    Some improvement to admin display

--- musicawardsapp/polls/admin.py
+++ musicawardsapp/polls/admin.py
@@ -4,5 +4,19 @@ from .models import Question
 from .models import Choice
 
 # Register your models here.
-admin.site.register(Question)
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ["pub_date", "question_text"]
+    inlines = [ChoiceInline]
+    list_display = ("question_text", "pub_date", "was_published_recently")
+    list_filter = ["pub_date"]
+    search_fields = ["question_text"]
+
+
+admin.site.register(Question,QuestionAdmin)
 admin.site.register(Choice)
